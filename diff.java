a4630ccecf05d0dc3962465ef676c62098a8abbc
alok singh thakur <alokthakur08cse@gmail.com>
2022-07-23 12:00:42 +0530

    made changes in diff file

--- diff.java
+++ diff.java
@@ -2,6 +2,7 @@ public class diff {
     public static void main(String[] args) {
         int a = 5;
         int b = 6;
+        int c = 7;
         System.out.println("the sum is" + (a - b));
     }
 }
